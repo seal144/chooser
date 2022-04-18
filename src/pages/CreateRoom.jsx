d57235426669d31e2d3887261e3b00f3c04562a3
Rafal F <functionalweasel@gmail.com>
2022-04-18 09:51:55 +0200

    feat: create crateRoom page

--- src/pages/CreateRoom.jsx
+++ src/pages/CreateRoom.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Input, Button } from 'antd';
 
 const CreateRoom = () => {
-    return ( 
-        <p>
-            CreateRoom
-        </p>
+    const [ roomForm, setRoomForm ] = useState({
+        hostName: '',
+        roomName: '',
+        roomPassword: ''
+    })
+
+    const updateFormState = (changedValue) => {
+        setRoomForm( prevState => { return {...prevState, ...changedValue}});
+    }
+
+    const onFinish = () => {
+        console.log('finished', roomForm);
+    }
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+    }
+
+    return (
+        <Form
+            {...layout}
+            name = "newRoom"
+            onFinish = {onFinish}
+            onValuesChange = { updateFormState }
+            autoComplete = "off"
+        >
+            <Form.Item 
+                label="Twoje Imię"
+                name="hostName"
+            >
+                <Input/>
+            </Form.Item>
+
+            <Form.Item 
+                label="Nazwa Pokoju"
+                name="roomName"
+            >
+                <Input/>
+            </Form.Item>
+
+            <Form.Item 
+                label="Hasło"
+                name="roomPassword"
+            >
+                <Input.Password/>
+            </Form.Item>
+
+            <Form.Item>
+            <Button type="primary" htmlType="submit">
+                Stwórz Pokój
+            </Button>
+            </Form.Item>
+        </Form>
      );
 }
  
