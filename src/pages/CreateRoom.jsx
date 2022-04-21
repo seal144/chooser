3a615d37cd4cca53cd78de28008391f820ca4abd
Rafal F <functionalweasel@gmail.com>
2022-04-21 19:09:56 +0200

    feat add options to crateRoom

--- src/pages/CreateRoom.jsx
+++ src/pages/CreateRoom.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
+import { CloseSquareOutlined, PlusOutlined } from '@ant-design/icons'
 
 const CreateRoom = () => {
     const [ roomForm, setRoomForm ] = useState({
         hostName: '',
         roomName: '',
-        roomPassword: ''
+        roomPassword: '',
+        options: []
     })
 
-    const updateFormState = (changedValue) => {
-        setRoomForm( prevState => { return {...prevState, ...changedValue}});
+    const updateFormState = (_, formValues) => {
+        setRoomForm( formValues );
     }
 
     const onFinish = () => {
@@ -17,12 +19,12 @@ const CreateRoom = () => {
     }
 
     const inputLayout = {
-        labelCol: { sm: { span: 7 }, md: { span:5 } },
-        wrapperCol: { sm: { span: 10 }, md: { span:14 } },
+        labelCol: { sm: { span: 6 }, md: { span:5 } },
+        wrapperCol: { sm: { span: 12 }, md: { span:14 } },
     }
 
     const buttonLayout = {
-        wrapperCol: { sm: { offset:7, span: 10 }, md: { offset:5, span: 14}},
+        wrapperCol: { sm: { offset:6, span: 12 }, md: { offset:5, span: 14}},
     }
 
     return (
@@ -37,10 +39,9 @@ const CreateRoom = () => {
                 label="Twoje Imię"
                 name="hostName"
                 rules={[
-                    { required: true, message: 'Podaj imię' },
+                    { required: true,whitespace: true, message: 'Podaj imię' },
                     { min: 2, message: 'Za krótkie imię'},
                     { max: 15, message: 'za długie imię'},
-                    { whitespace: true, message: 'Niepoprawne imię'},
                 ]}
             >
                 <Input/>
@@ -50,10 +51,9 @@ const CreateRoom = () => {
                 label="Nazwa Pokoju"
                 name="roomName"
                 rules={[
-                    { required: true, message: 'Podaj nazwę pokoju' },
+                    { required: true, whitespace: true, message: 'Podaj nazwę pokoju' },
                     { min: 2, message: 'Za krótka nazwa'},
                     { max: 15, message: 'za długa nazwa'},
-                    { whitespace: true, message: 'Niepoprawne hasło'},
                 ]}
             >
                 <Input/>
@@ -65,6 +65,65 @@ const CreateRoom = () => {
             >
                 <Input.Password/>
             </Form.Item>
+
+            <Form.List
+                name="options"
+                rules={[
+                    {
+                        validator: async (_, options) => {
+                            if (!options || options.length < 2) {
+                                return Promise.reject(new Error('Minimum dwie opcje'));
+                            }
+                        },
+                    },
+                ]}
+            >
+                {(fields, { add, remove }, { errors }) => (
+                    <>
+                        {fields.map((field, index) => (
+                            <Form.Item
+                                {...(index === 0 ? inputLayout : buttonLayout)}
+                                label={index === 0 ? 'Opcje' : ''}
+                                required={false}
+                                key={field.key}
+                            >
+                            <Form.Item
+                                {...field}
+                                validateTrigger={['onChange', 'onBlur']}
+                                rules={[
+                                    {
+                                        required: true,
+                                        whitespace: true,
+                                        message: "Wpisz opcję lub usuń pole",
+                                    },
+                                ]}
+                                noStyle
+                            >
+                            <Input placeholder="Opcja" style={{ width: '90%' }} />
+                            </Form.Item>
+                                {fields.length > 0 ? (
+                                    <CloseSquareOutlined
+                                    className="dynamic-delete-button"
+                                    onClick={() => remove(field.name)}
+                                    style={{marginLeft: "5px"}}
+                                    />
+                                ) : null}
+                            </Form.Item>
+                        ))}
+                        <Form.Item {...buttonLayout}>
+                            <Button
+                                type="dashed"
+                                onClick={() => add()}
+                                icon={<PlusOutlined />}
+                                block
+                            >
+                            Dodaj opcję
+                            </Button>
+                            <Form.ErrorList errors={errors} />
+                        </Form.Item>
+                    </>
+                )}
+            </Form.List>
 
             <Form.Item {...buttonLayout}>
             <Button type="primary" block htmlType="submit">
