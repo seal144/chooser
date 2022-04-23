d1bf519dbdb857db7af07ffcd85716a84c27324f
Rafal F <functionalweasel@gmail.com>
2022-04-23 11:18:07 +0200

    feat: add room admin url

--- src/helpers/paths.js
+++ src/helpers/paths.js
@@ -2,4 +2,5 @@ export const HOME = '/';
 export const CREATEROOM = '/create-room';
 export const JOINROOM = '/join-room';
 export const ROOM = '/room/:roomName';
+export const ROOMADMIN = '/room/admin/:roomName'
 export const ABOUT = '/ABOUT';
--- src/layouts/Head.jsx
+++ src/layouts/Head.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, useParams, Navigate } from "react-router-dom";
 
-import { HOME, ABOUT, CREATEROOM, JOINROOM, ROOM } from '../helpers/paths';
+import { HOME, ABOUT, CREATEROOM, JOINROOM, ROOM, ROOMADMIN } from '../helpers/paths';
 
 const Head = () => {
     return (
@@ -10,6 +10,7 @@ const Head = () => {
           <Route path={ ABOUT } element={<h2 className="header">O APLIKACJI</h2>}/>
           <Route path={ CREATEROOM } element={<h2 className="header">STWÓRZ POKÓJ</h2>}/>
           <Route path={ JOINROOM } element={<h2 className="header">DOŁĄCZ DO POKOJU</h2>}/>
+          <Route path={ ROOMADMIN } element={<HeaderRoom/>}/>
           <Route path={ ROOM } element={<HeaderRoom/>}/>
           <Route path="*" element={<Navigate to="/" replace={true} />}/>
         </Routes>
